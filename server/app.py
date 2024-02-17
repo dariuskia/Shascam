1c055ffa78bc3e4c233b8113feeb0f8177864271
Darius Kianersi <dariuskianersi@gmail.com>
2024-02-17 03:58:33 -0800

    remove comments

--- server/app.py
+++ server/app.py
@@ -24,35 +24,6 @@ def echo(ws):
     while True:
         data = ws.receive()
         print(type(data), data)
-        """
-        if message is None:
-            app.logger.info("No message received...")
-            continue
-
-        # Messages are a JSON encoded string
-        data = json.loads(message)
-
-        # Using the event type you can determine what type of message you are receiving
-        if data['event'] == "connected":
-            app.logger.info("Connected Message received: {}".format(message))
-        if data['event'] == "start":
-            app.logger.info("Start Message received: {}".format(message))
-        if data['event'] == "media":
-            if not has_seen_media:
-                app.logger.info("Media message: {}".format(message))
-                payload = data['media']['payload']
-                app.logger.info("Payload is: {}".format(payload))
-                chunk = base64.b64decode(payload)
-                app.logger.info("That's {} bytes".format(len(chunk)))
-                app.logger.info("Additional media messages from WebSocket are being suppressed....")
-                has_seen_media = True
-        if data['event'] == "closed":
-            app.logger.info("Closed Message received: {}".format(message))
-            break
-        message_count += 1
-        """
-
-    app.logger.info("Connection closed. Received a total of {} messages".format(message_count))
 
 
 if __name__ == '__main__':
