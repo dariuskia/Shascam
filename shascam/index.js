248ee6521e056b3509743e0e22da097d1507113a
Darius Kianersi <dariuskianersi@gmail.com>
2024-02-24 19:18:02 -0500

    add new react native app

--- shascam/index.js
+++ shascam/index.js
@@ -1,15 +1,9 @@
-import { registerRootComponent } from 'expo';
+/**
+ * @format
+ */
 
+import {AppRegistry} from 'react-native';
 import App from './App';
+import {name as appName} from './app.json';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-import { AppRegistry } from 'react-native';
-try{
-    AppRegistry.registerComponent('main',() => App);
-}
-catch(error){
-    //console.log(error.stack)
-}
-
+AppRegistry.registerComponent(appName, () => App);
